4f57ccbf7ee1f4fd3f4a30c4934dcacdfa9bee73
oxun_123 <r3331426!@#>
2023-04-30 18:48:15 +0600

    datagrid

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -5,14 +5,14 @@ const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
 
-    return <Outlet />;
+    // return <Outlet />;
 
-    // return (
-            // auth?.roles.find(role => allowedRoles?.includes(role)) != null ? <Outlet />
-            //                                                                : 
-            // auth?.user ? <Navigate to="/unauthorized" state={{ from: location }} replace />
-            //             : <Navigate to="/login" state={{ from: location }} replace />
-            // );
+    return (
+            auth?.roles.find(role => allowedRoles?.includes(role)) != null ? <Outlet />
+                                                                           : 
+            auth?.user ? <Navigate to="/unauthorized" state={{ from: location }} replace />
+                        : <Navigate to="/login" state={{ from: location }} replace />
+            );
 }
 
 export default RequireAuth;
